8578958340ac754a38bd4ff00715482c2b6299d8
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-05 00:10:41 +0600

    Routing path setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Volunteers from './Components/Volunteers/Volunteers';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/volunteers' element={<Volunteers></Volunteers>}></Route>
+      </Routes>
     </div>
   );
 }
