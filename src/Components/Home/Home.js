d5dad1bc90096c8ec271de396853362500af5fa3
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-05 00:28:07 +0600

    Practice useState with count number

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';
 
 const Home = () => {
+    const [count, setCount] = useState(0);
+    const number = 60;
+
+    const increaseCount = () => {
+        setCount(count + 1);
+    }
+
     return (
         <div>
-            
+            <h4>My Number: {number}</h4>
+            <p>Count: {count}</p>
+            <button onClick={increaseCount}>Click Me</button>
+            <p><small>Double Count: {count * 2}</small></p>
         </div>
     );
 };
