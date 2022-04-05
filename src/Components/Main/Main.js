a47af378c70fbdf5589ca9d09b2d8b1fadc5f4ee
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-05 12:30:05 +0600

    Implement Search with simple code

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import useVolunteers from '../../hooks/useVolunteers';
 import Activity from '../Activity/Activity';
 import './Main.css';
@@ -5,12 +6,24 @@ import './Main.css';
 const Main = () => {
     const [volunteers, setVolunteers] = useVolunteers();
 
+    const [searchResult, setSearchResult] = useState([]);
+
+    const handleSearchChange = (event) => {
+        const searchText = event.target.value;
+        //convert smaller case letter to match search
+        const match = volunteers.filter(v => v.title.includes(searchText));
+        setSearchResult(match);
+    }
+
     return (
         <div>
             <h2>Volunteer Acitivities: {volunteers.length}</h2>
+            <div style={{ margin: '20px' }}>
+                <input onChange={handleSearchChange} type="text" placeholder='Search' />
+            </div>
             <div className="activity-container">
                 {
-                    volunteers.map(activity => <Activity
+                    searchResult.map(activity => <Activity
                         key={activity._id}
                         activity={activity}
                     ></Activity>)
