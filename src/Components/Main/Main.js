d95143d596e966ed659d46af56d81613e0080b0f
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-05 12:44:25 +0600

    Use Effect with dependency List

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,23 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useVolunteers from '../../hooks/useVolunteers';
 import Activity from '../Activity/Activity';
 import './Main.css';
 
 const Main = () => {
-    const [volunteers, setVolunteers] = useVolunteers();
-
+    // const [volunteers, setVolunteers] = useVolunteers();
+    const [searchText, setSearchText] = useState('');
     const [searchResult, setSearchResult] = useState([]);
 
+    useEffect(() => {
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => {
+            const match = data.filter(d => d.title.includes(searchText));
+            setSearchResult(match);
+        })
+    }, [searchText]);
+
+
+
     const handleSearchChange = (event) => {
-        const searchText = event.target.value;
-        //convert smaller case letter to match search
-        const match = volunteers.filter(v => v.title.includes(searchText));
-        setSearchResult(match);
+        setSearchText(event.target.value);
+
     }
+
+    // const handleSearchChange2 = (event) => {
+    //     const searchText = event.target.value;
+    //     //convert smaller case letter to match search
+    //     const match = volunteers.filter(v => v.title.includes(searchText));
+    //     setSearchResult(match);
+    // }
 
     return (
         <div>
-            <h2>Volunteer Acitivities: {volunteers.length}</h2>
+            <h2>Volunteer Acitivities: {searchResult.length}</h2>
             <div style={{ margin: '20px' }}>
                 <input onChange={handleSearchChange} type="text" placeholder='Search' />
             </div>
